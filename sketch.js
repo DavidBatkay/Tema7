99ca9c0b609cd1d6013cf5e629a92aa693817896
DavidBatkay <146411522+DavidBatkay@users.noreply.github.com>
2024-03-27 17:23:19 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let sunSize = 50;
 let planetSizes = [8, 12, 15, 18, 22];
 let orbitSizes = [80, 120, 160, 200, 240];
 let planetColors = ['#FF5733', '#33FF57', '#5733FF', '#FF33D1', '#33FFFF'];
-let planetAngles = []; // Array to store random starting angles for each planet
+let planetAngles = []; 
 let angleX = 0;
 let angleY = 0;
 let sensitivity = 0.01;
@@ -11,37 +11,37 @@ let zoom = -500;
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   
-  // Generate random starting angles for each planet
+  
   for (let i = 0; i < planetSizes.length; i++) {
-    planetAngles.push(random(TWO_PI)); // Random angle within 0 to 2*PI
+    planetAngles.push(random(TWO_PI)); 
   }
 }
 
 function draw() {
   background(0);
   
-  // Apply zoom
+ 
   translate(0, 0, zoom);
   
-  // Rotate the camera
+  
   rotateX(angleX);
   rotateY(angleY);
   
-  // Draw sun
-  fill(255, 255, 0); // yellow
+  
+  fill(255, 255, 0); 
   noStroke();
   sphere(sunSize);
 
-  // Draw orbits
+  
   noFill();
   stroke(255, 150);
   for (let i = 0; i < orbitSizes.length; i++) {
     ellipse(0, 0, orbitSizes[i] * 2, orbitSizes[i] * 2);
   }
 
-  // Draw planets
+  
   for (let i = 0; i < planetSizes.length; i++) {
-    let angle = planetAngles[i] + millis() * 0.0001; // Add time-based rotation to make them move
+    let angle = planetAngles[i] + millis() * 0.0001; 
     let x = orbitSizes[i] * cos(angle);
     let y = orbitSizes[i] * sin(angle);
 
@@ -64,5 +64,5 @@ function mouseDragged() {
 
 function mouseWheel(event) {
   zoom += event.delta;
-  return false; // Prevent default
+  return false; 
 }
